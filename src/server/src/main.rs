7367bd6f57636b716c55b545b4163598280b0660
da-the-dev <leshiktk@me.com>
2022-07-22 19:32:19 +0300

    Moved code to modules

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,62 +1,30 @@
-use actix_web::{get, post, web, App, HttpResponse, Result, HttpServer, Responder, http::header, body};
-use actix_files as fs;
-use fs::NamedFile;
+use actix_web::{web, App, Result, HttpServer, HttpResponse};
+use actix_files::{Files, NamedFile};
+mod paths; use paths::identify::identify as identify;
+use serde::Deserialize;
+
+// #[derive(Deserialize)]
+// pub struct Code {
+//     code: String
+// }
+
+// async fn test(query: web::Query<Code>) -> Result<HttpResponse> {
+//     Ok(HttpResponse::Ok().body(query.code.to_string()))
+// }
 
 async fn index() -> Result<NamedFile> {
-    Ok(fs::NamedFile::open("./public/index.html")?)
-}
-
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-struct Code {
-    code: String
-}
-
-#[derive(Serialize, Deserialize)]
-struct IdentifyRequest {
-    client_id: String,
-    client_secret: String,
-    grant_type: String,
-    code: String,
-    redirect_uri: String
-}
-
-#[get("/identify")] 
-async fn identify(req_body: web::Json<Code>) -> impl Responder {
-    // let code = &req_body.code;
-    let body = IdentifyRequest {
-        client_id: String::from("997526709148598282"),
-        client_secret: String::from("expunged"),
-        grant_type: String::from("authorization_code"),
-        code: String::from("expunged"),
-        redirect_uri: String::from("http://localhost:8000/")
-    };
-
-
-    let client = reqwest::Client::new();
-    let resp = client
-        .post("https://discord.com/api/v10/oauth2/token")
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(serde_urlencoded::to_string(body).unwrap())
-        .send();
-    match resp.await {
-        Ok(r) => {
-            match r.text().await {
-                Ok(t) => HttpResponse::Accepted().body(t),
-                Err(e) => HttpResponse::BadGateway().body(format!("Error in r.text: {}", e))
-            }
-        },
-        Err(e) => HttpResponse::BadGateway().body(format!("Error in responce: {}", e))
-    }
+    Ok(NamedFile::open("./public/index.html")?)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
     HttpServer::new(|| {
         App::new()
+            // .route("/test", web::get().to(test))
             .route("/", web::get().to(index))
-            .service(fs::Files::new("/", "./public").show_files_listing())
+            .service(identify)
+            .service(Files::new("/", "./public").show_files_listing())
     })
     .bind(("127.0.0.1", 8000))?
     .run()
---
+++ src/server/src/paths/identify.rs
@@ -0,0 +1,51 @@
+use actix_web::{get, web, HttpResponse, Result, Responder};
+use serde::{Deserialize, Serialize};
+use std::{error::Error, env};
+use serde_urlencoded::to_string;
+
+#[derive(Serialize, Deserialize)]
+pub struct Code {
+    code: String
+}
+#[derive(Serialize, Deserialize)]
+pub struct IdentifyRequest {
+    client_id: String,
+    client_secret: String,
+    grant_type: String,
+    code: String,
+    redirect_uri: String
+}
+
+#[get("/identify")] 
+pub async fn identify(info: web::Query<Code>) -> Result<HttpResponse, Box<dyn Error>> {
+    // Create the request to get user access code
+    let code = &info.code;
+    let body = IdentifyRequest {
+        client_id: String::from("997526709148598282"),
+        client_secret: String::from(env::var("CLIENT_SECRET")?),
+        grant_type: String::from("authorization_code"),
+        code: code.to_string(),
+        redirect_uri: String::from("http://localhost:8000/")
+    };
+
+    // Complete the request
+    let client = reqwest::Client::new();
+    let resp = client
+        .post("https://discord.com/api/v10/oauth2/token")
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(to_string(body)?)
+        .send();
+
+    match resp.await {
+        Ok(r) => {
+            match r.text().await {
+                Ok(t) => {
+                    println!("{}", t);
+                    Ok(HttpResponse::Ok().body(t))
+                },
+                Err(e) => Ok(HttpResponse::BadGateway().body(format!("Error in r.text: {}", e)))
+            }
+        },
+        Err(e) => Ok(HttpResponse::BadGateway().body(format!("Error in responce: {}", e)))
+    }
+}
